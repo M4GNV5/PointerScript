bd3fecd6df6c1643aafd9d52310c3e50791e98cb
M4GNV5 <eragonjml@googlemail.com>
2016-02-18 15:56:27 +0100

    dont crash if source file doesnt exist

--- src/main.c
+++ src/main.c
@@ -4,18 +4,32 @@
 
 int main(int argc, char **argv)
 {
-	FILE *fd = fopen(argv[1], "rb");
-	fseek(fd, 0, SEEK_END);
-	long fsize = ftell(fd);
-	fseek(fd, 0, SEEK_SET);
+	if(argc == 1)
+	{
+		//TODO interactive mode
+	}
+	else if(argc == 2)
+	{
+		FILE *fd = fopen(argv[1], "r");
 
-	char *src = malloc(fsize + 1);
-	fread(src, fsize, 1, fd);
-	fclose(fd);
-	src[fsize] = 0;
+		if(fd == NULL)
+		{
+			fprintf(stderr, "Failed to open file %s\n", argv[1]);
+			return 2;
+		}
 
-	ptrs_ast_t *ast = parse(src);
-	ast->handler(ast);
+		fseek(fd, 0, SEEK_END);
+		long fsize = ftell(fd);
+		fseek(fd, 0, SEEK_SET);
+
+		char *src = malloc(fsize + 1);
+		fread(src, fsize, 1, fd);
+		fclose(fd);
+		src[fsize] = 0;
+
+		ptrs_ast_t *ast = parse(src);
+		ast->handler(ast);
+	}
 
 	return 0;
 }
