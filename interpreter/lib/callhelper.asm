1d06ecdfb0ee35ebe9796c2c0b89d0a62213a5b8
M4GNV5 <eragonjml@googlemail.com>
2016-03-19 01:04:49 +0100

    fix byte alignment in callhelper

--- interpreter/lib/callhelper.asm
+++ interpreter/lib/callhelper.asm
@@ -6,11 +6,11 @@ section .text
 							; RDI, RSI, RDX, RCX, R8, R9
 							; XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6, XMM7
 ptrs_call_amd64ABI:			; intptr_t ptrs_call_amd64ABI(void *func, int64_t *intArgv, uint8_t floatArgc, double *floatArgv);
-	sub		rsp, 25
+	sub		rsp, 32
 	mov		[rsp], rdi		; func
 	mov		[rsp + 8], rsi	; intArgv
 	mov		[rsp + 16], dl	; floatArgc
-	mov		[rsp + 17], rcx	; floatArgv
+	mov		[rsp + 24], rcx	; floatArgv
 
 	mov		rax, qword [rsp + 8]
 	mov		rdi, qword [rax]
@@ -20,20 +20,21 @@ ptrs_call_amd64ABI:			; intptr_t ptrs_call_amd64ABI(void *func, int64_t *intArgv
 	mov		r8, qword [rax + 32]
 	mov		r9, qword [rax + 40]
 	
-	mov		rax, qword [rsp + 17]
+	mov		rax, qword [rsp + 24]
 	movsd	XMM0, qword [rax]
 	movsd	XMM1, qword [rax + 8]
 	movsd	XMM2, qword [rax + 16]
 	movsd	XMM3, qword [rax + 24]
 	movsd	XMM4, qword [rax + 32]
 	movsd	XMM5, qword [rax + 40]
-	movsd	XMM6, qword [rax + 40]
-	movsd	XMM7, qword [rax + 40]
+	movsd	XMM6, qword [rax + 48]
+	movsd	XMM7, qword [rax + 56]
 	
 	mov		rdx, qword [rsp]
 	mov		al, byte [rsp + 16]
-	add		rsp, 25
+	add		rsp, 24
 	call	rdx
+	add		rsp, 8
 	ret
 	
 	
