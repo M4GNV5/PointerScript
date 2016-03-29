6b7d766f3c9d47b28966247cdac65fabd0a766c4
M4GNV5 <eragonjml@googlemail.com>
2016-03-30 01:03:01 +0200

    remove old not 100% working dynamic call implementation

--- interpreter/lib/call.c
+++ interpreter/lib/call.c
@@ -15,40 +15,6 @@ ptrs_var_t *ptrs_callfunc(ptrs_function_t *func, ptrs_var_t *result, int argc, p
 	return result;
 }
 
-#define nativecaller_create(n, ...)\
-	intptr_t ptrs_callnative_##n(ptrs_nativefunc_t func, intptr_t *args) \
-	{ \
-		return func(__VA_ARGS__); \
-	}
-
-nativecaller_create(0, 0)
-nativecaller_create(1, args[0])
-nativecaller_create(2, args[0], args[1])
-nativecaller_create(3, args[0], args[1], args[2])
-nativecaller_create(4, args[0], args[1], args[2], args[3])
-nativecaller_create(5, args[0], args[1], args[2], args[3], args[4])
-nativecaller_create(6, args[0], args[1], args[2], args[3], args[4], args[5])
-nativecaller_create(7, args[0], args[1], args[2], args[3], args[4], args[5], args[6])
-nativecaller_create(8, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
-nativecaller_create(9, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
-nativecaller_create(10, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9])
-
-
-typedef intptr_t (*nativecaller_t)(ptrs_nativefunc_t, intptr_t *);
-nativecaller_t caller[] = {
-	ptrs_callnative_0,
-	ptrs_callnative_1,
-	ptrs_callnative_2,
-	ptrs_callnative_3,
-	ptrs_callnative_4,
-	ptrs_callnative_5,
-	ptrs_callnative_6,
-	ptrs_callnative_7,
-	ptrs_callnative_8,
-	ptrs_callnative_9,
-	ptrs_callnative_10,
-};
-
 //see callhelper.asm
 intptr_t ptrs_call_amd64ABI(void *func, int64_t *intArgv, uint8_t floatArgc, double *floatArgv);
 
