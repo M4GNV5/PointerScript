72f721512276d3998c91bd842468fc7e7f79961a
M4GNV5 <eragonjml@googlemail.com>
2016-08-31 18:40:04 +0200

    remove the (broken) alias statement

--- parser/ast.c
+++ parser/ast.c
@@ -378,17 +378,9 @@ static ptrs_ast_t *parseStatement(code_t *code)
 		consumec(code, '=');
 		ptrs_ast_t *ast = parseExpression(code);
 		if(ast->handler != PTRS_HANDLE_CONSTANT)
-			unexpectedm(code, NULL, "Initializer for 'const' variable is not a constant. Use 'alias' instead");
+			unexpectedm(code, NULL, "Initializer for 'const' variable is not a constant");
 
 		addSpecialSymbol(code, name, ast);
-		consumec(code, ';');
-		return NULL;
-	}
-	else if(lookahead(code, "alias"))
-	{
-		char *name = readIdentifier(code);
-		consumec(code, '=');
-		addSpecialSymbol(code, name, parseExpression(code));
 		consumec(code, ';');
 		return NULL;
 	}
