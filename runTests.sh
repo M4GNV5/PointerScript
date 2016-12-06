c107ae1e235f2be901227eda0b82778f9feae20e
M4GNV5 <eragonjml@googlemail.com>
2016-12-06 22:37:59 +0100

    first step in adding tests (types, trycatch and stringformat for now)

---
+++ runTests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+nocolor='\033[0m'
+
+function runTest
+{
+	printf "${yellow}TRYING${nocolor} test $1\n"
+	bin/ptrs tests/$1.ptrs
+
+	local status=$?
+	if [ $status -ne 0 ]; then
+		printf "\n${red}ERROR${nocolor} running test $1\n"
+	else
+		printf "\e[1A${green}SUCCESS${nocolor} running test $1\n"
+	fi
+}
+
+runTest runtime/types
+runTest runtime/trycatch
+runTest runtime/strformat
