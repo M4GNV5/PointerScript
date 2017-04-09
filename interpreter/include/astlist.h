249656d00bbbcda150c433a96f26518796c28696
M4GNV5 <jakob@m4gnus.de>
2017-04-09 18:18:50 +0200

    remove the inerpreter dir

--- interpreter/include/astlist.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PTRS_ASTLIST
-#define _PTRS_ASTLIST
-
-int ptrs_astlist_length(struct ptrs_astlist *list, ptrs_ast_t *node, ptrs_scope_t *scope);
-void ptrs_astlist_handle(struct ptrs_astlist *list, int len, ptrs_var_t *out, ptrs_scope_t *scope);
-void ptrs_astlist_handleByte(struct ptrs_astlist *list, int len, uint8_t *out, ptrs_scope_t *scope);
-
-#endif
--- interpreter/include/run.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PTRS_RUN
-#define _PTRS_RUN
-
-#include "../../parser/common.h"
-
-ptrs_ast_t *ptrs_eval(char *src, const char *filename, ptrs_var_t *result, ptrs_scope_t *scope, ptrs_symboltable_t **symbols);
-ptrs_ast_t *ptrs_dofile(const char *file, ptrs_var_t *result, ptrs_scope_t *scope, ptrs_symboltable_t **symbols);
-
-#endif
