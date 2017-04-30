183e5ea188257dc38488fe7cb3b8950c439d5884
M4GNV5 <jakob@m4gnus.de>
2017-05-01 00:41:21 +0200

    fill arrays with the last provided init val instead of undefined/0

--- jit/lib/astlist.c
+++ jit/lib/astlist.c
@@ -7,19 +7,18 @@
 
 void ptrs_astlist_handle(struct ptrs_astlist *list, long valReg, long sizeReg, jit_state_t *jit, ptrs_scope_t *scope)
 {
-	const ptrs_meta_t defaultMetaVal = {.type = PTRS_TYPE_UNDEFINED};
-
 	int i;
+	unsigned result;
+
 	for(i = 0; list != NULL; i += 16)
 	{
 		//if(list->expand) //TODO
 
-		unsigned result;
 		if(list->entry == NULL)
 		{
 			result = scope->usedRegCount;
 			jit_movi(jit, R(result), 0);
-			jit_movi(jit, R(result + 1), *(uintptr_t *)&defaultMetaVal);
+			jit_movi(jit, R(result + 1), 0);
 		}
 		else
 		{
@@ -32,20 +31,18 @@ void ptrs_astlist_handle(struct ptrs_astlist *list, long valReg, long sizeReg, j
 		list = list->next;
 	}
 
-	long iReg = R(scope->usedRegCount++);
-	long defaultVal = R(scope->usedRegCount++);
-	long defaultMeta = R(scope->usedRegCount++);
+	assert(result == scope->usedRegCount);
+	scope->usedRegCount += 2;
 
+	long iReg = R(scope->usedRegCount++);
 	jit_movi(jit, iReg, i);
-	jit_movi(jit, defaultVal, 0);
-	jit_movi(jit, defaultMeta, *(uintptr_t *)&defaultMetaVal);
 
 	jit_label *check = jit_get_label(jit);
 	jit_op *done = jit_bger_u(jit, JIT_FORWARD, iReg, sizeReg); //while(i < array.size)
 
-	jit_stxr(jit, valReg, iReg, defaultVal, 8); //array[i] = default.value
+	jit_stxr(jit, valReg, iReg, R(result), 8); //array[i] = default.value
 	jit_addi(jit, iReg, iReg, 8);         //i += 8
-	jit_stxr(jit, valReg, iReg, defaultMeta, 8); //array[i] = default.meta
+	jit_stxr(jit, valReg, iReg, R(result + 1), 8); //array[i] = default.meta
 	jit_addi(jit, iReg, iReg, 8);         //i += 8
 
 	jit_jmpi(jit, check);
@@ -57,11 +54,12 @@ void ptrs_astlist_handle(struct ptrs_astlist *list, long valReg, long sizeReg, j
 void ptrs_astlist_handleByte(struct ptrs_astlist *list, long valReg, long sizeReg, jit_state_t *jit, ptrs_scope_t *scope)
 {
 	int i;
+	unsigned result;
+
 	for(i = 0; list != NULL; i++)
 	{
 		//if(list->expand) //TODO
 
-		unsigned result;
 		if(list->entry == NULL)
 		{
 			result = scope->usedRegCount;
@@ -73,7 +71,7 @@ void ptrs_astlist_handleByte(struct ptrs_astlist *list, long valReg, long sizeRe
 			ptrs_jit_convert(jit, ptrs_vartoi, R(result), R(result), R(result + 1));
 		}
 
-		jit_stxi(jit, i, valReg, result, 1);
+		jit_stxi(jit, i, valReg, R(result), 1);
 
 		list = list->next;
 	}
@@ -83,7 +81,7 @@ void ptrs_astlist_handleByte(struct ptrs_astlist *list, long valReg, long sizeRe
 
 	jit_prepare(jit);
 	jit_putargr(jit, valReg);
-	jit_putargi(jit, 0);
+	jit_putargr(jit, R(result));
 	jit_putargr(jit, tmp);
 	jit_call(jit, memset);
 }
