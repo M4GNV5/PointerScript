845259a2b89ed8b770cf3f167f9052c63251aae1
Jakob Löw <jakob@m4gnus.de>
2017-11-22 14:20:48 +0100

    rewrite the slice expression to use the type switch macro magic

--- jit/specialexpr.c
+++ jit/specialexpr.c
@@ -410,24 +410,28 @@ ptrs_jit_var_t ptrs_handle_slice(ptrs_ast_t *node, jit_function_t func, ptrs_sco
 	jit_value_t oldSize = scope->indexSize;
 	scope->indexSize = ptrs_jit_getArraySize(func, val.meta);
 
-	ptrs_jit_var_t _start = expr->start->handler(expr->start, func, scope);
-	jit_value_t start = ptrs_jit_vartoi(func, _start);
-
-	ptrs_jit_var_t _end = expr->end->handler(expr->end, func, scope);
-	jit_value_t end = ptrs_jit_vartoi(func, _end);
+	jit_value_t start = ptrs_jit_vartoi(func,
+		expr->start->handler(expr->start, func, scope));
+	jit_value_t end = ptrs_jit_vartoi(func,
+		expr->end->handler(expr->end, func, scope));
 
 	jit_value_t newSize = jit_insn_sub(func, end, start);
 	jit_value_t newPtr = jit_value_create(func, jit_type_void_ptr);
 
-	jit_label_t done = jit_label_undefined;
-	jit_insn_store(func, newPtr, jit_insn_load_elem_address(func, val.val, start, jit_type_ubyte));
-	jit_insn_branch_if(func, jit_insn_eq(func, type, jit_const_int(func, ulong, PTRS_TYPE_NATIVE)), &done);
+	ptrs_jit_typeSwitch(node, func, scope, val,
+		(1, "Cannot slice variable of type %t", TYPESWITCH_TYPE),
+		(PTRS_TYPE_NATIVE, PTRS_TYPE_POINTER),
 
-	ptrs_jit_assert(node, func, scope, jit_insn_eq(func, type, jit_const_int(func, ulong, PTRS_TYPE_POINTER)),
-		1, "Cannot slice variable of type %t", type);
-	jit_insn_store(func, newPtr, jit_insn_load_elem_address(func, val.val, start, jit_type_ubyte));
+		case PTRS_TYPE_NATIVE:
+			jit_insn_store(func, newPtr,
+				jit_insn_load_elem_address(func, val.val, start, jit_type_ubyte));
+			break;
 
-	jit_insn_label(func, &done);
+		case PTRS_TYPE_POINTER:
+			jit_insn_store(func, newPtr,
+				jit_insn_load_elem_address(func, val.val, start, ptrs_jit_getVarType()));
+			break;
+	);
 
 	/*
 	disabled so we can slice arrays received from native functions.
